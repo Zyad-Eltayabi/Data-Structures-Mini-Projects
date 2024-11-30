032ca5000ef91f16878da0dfa0a9cd5c62856180
ZYAD-19 <zyadmahmod19720@gmail.com>
2024-11-30 18:00:09 +0200

    implement doubly linked list

--- Program.cs
+++ Program.cs
@@ -1,10 +1,50 @@
-﻿namespace Data_Structures_Mini_Projects
+﻿using DoublyLinkedList;
+
+namespace Data_Structures_Mini_Projects
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Test doubly linked list
+            MyDoublyLinkedList<int> myDoublyLinkedList = new MyDoublyLinkedList<int>();
+
+            myDoublyLinkedList.InsertAtFront(5);
+            myDoublyLinkedList.InsertAtFront(4);
+            myDoublyLinkedList.InsertAtFront(3);
+            myDoublyLinkedList.InsertAtFront(2);
+            myDoublyLinkedList.InsertAtFront(1);
+
+            Console.WriteLine("Linked List Content");
+            myDoublyLinkedList.Print();
+
+            // searching for the node
+            DoublyLinkedList.Node<int> findNode = myDoublyLinkedList.Find(2);
+            if (findNode != null)
+                Console.WriteLine("node with value 2 is found :-) ");
+            else
+                Console.WriteLine("node with value 2 is not found :-(");
+
+            myDoublyLinkedList.InsertAfter(findNode, 500);
+            Console.WriteLine("after insert 500 after 2 :");
+            myDoublyLinkedList.Print();
+
+            myDoublyLinkedList.InsertAtEnd( 700);
+            Console.WriteLine("after insert 700 at end :");
+            myDoublyLinkedList.Print();
+
+            DoublyLinkedList.Node<int> findNode4 = myDoublyLinkedList.Find(4);
+            myDoublyLinkedList.DeleteNode(findNode4);
+            Console.WriteLine("after deleting node with value 4 :");
+            myDoublyLinkedList.Print();
+
+            myDoublyLinkedList.DeleteFirstNode();
+            Console.WriteLine("after deleting first node :");
+            myDoublyLinkedList.Print();
+
+            myDoublyLinkedList.DeleteLastNode();
+            Console.WriteLine("\nafter deleting last node :");
+            myDoublyLinkedList.Print();
         }
     }
 }
---
+++ project 1/MyDoublyLinkedList.cs
@@ -0,0 +1,161 @@
+﻿namespace DoublyLinkedList
+{
+    public class Node<T>
+    {
+        public T value;
+        public Node<T> next;
+        public Node<T> prev;
+
+        public Node(T value)
+        {
+            this.value = value;
+            next = prev = null;
+        }
+    }
+
+    public class MyDoublyLinkedList<T>
+    {
+
+        public Node<T> head = null;
+
+        public void InsertAtFront(T value)
+        {
+            Node<T> newNode = new Node<T>(value);
+
+            if (head == null)
+            {
+                head = newNode;
+                return;
+            }
+
+            Node<T> firstNode = head;
+            firstNode.prev = newNode;
+            newNode.next = firstNode;
+            newNode.prev = null;
+            head = newNode;
+        }
+
+        public void Print()
+        {
+            Node<T> current = head;
+            Console.WriteLine("_________________________");
+            while (current != null)
+            {
+                Console.WriteLine(current.value);
+                current = current.next;
+            }
+        }
+
+        public Node<T> Find(T value)
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                if (current.value.Equals(value))
+                    return current;
+                current = current.next;
+            }
+            return null;
+        }
+
+        public void InsertAfter(Node<T> prevNode, T newNodeValue)
+        {
+            if (prevNode == null)
+                return;
+
+            Node<T> newNode = new Node<T>(newNodeValue);
+            newNode.next = prevNode.next;
+            newNode.prev = prevNode;
+
+            if (prevNode.next != null)
+                prevNode.next.prev = newNode;
+
+            prevNode.next = newNode;
+        }
+
+        public void InsertAtEnd(T value)
+        {
+            Node<T> newNode = new Node<T>(value);
+
+            if (head == null)
+            {
+                head = newNode;
+                return;
+            }
+
+            Node<T> lastNode = head;
+            while (lastNode.next != null)
+            {
+                lastNode = lastNode.next;
+            }
+
+            lastNode.next = newNode;
+            newNode.prev = lastNode;
+        }
+
+        public void DeleteNode(Node<T> deletedNode)
+        {
+            if (deletedNode == null || head == null)
+                return;
+
+            if (deletedNode.prev != null && deletedNode.next != null)
+            {
+                deletedNode.prev.next = deletedNode.next;
+                deletedNode.next.prev = deletedNode.prev;
+                return;
+            }
+
+            // case if the node to delete is the last node
+            if (deletedNode.next == null)
+            {
+                deletedNode.prev.next = null;
+                deletedNode.prev = null;
+                return;
+            }
+
+            // case if the node to delete is the first node
+            if (deletedNode.prev == null)
+            {
+                deletedNode.next.prev = null;
+                head = deletedNode.next;
+                deletedNode.next = null;
+                return;
+            }
+        }
+
+        public void DeleteFirstNode()
+        {
+            if (head == null)
+                return;
+
+            Node<T> current = head;
+
+            if (current.next != null)
+                current.next.prev = null;
+
+            head = current.next;
+
+            current.next = null;
+        }
+
+        public void DeleteLastNode()
+        {
+            if (head == null)
+                return;
+
+            Node<T> current = head;
+            while (current.next != null)
+            {
+                current = current.next;
+            }
+
+            if (current.prev != null)
+                current.prev.next = null;
+            else
+                head = null;
+        }
+    }
+
+
+
+}
