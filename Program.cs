cd290d733aa15047409543d43dee3d770b45d770
ZYAD-19 <zyadmahmod19720@gmail.com>
2024-11-30 16:43:28 +0200

    add Program.cs file

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Data_Structures_Mini_Projects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
