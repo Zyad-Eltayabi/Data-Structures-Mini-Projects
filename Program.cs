ade7e936ec76e22196eefccad1fcd308b2e4ee96
ZYAD-19 <zyadmahmod19720@gmail.com>
2024-12-03 03:31:01 +0200

    start implementing Dynamic Array data structure

--- Program.cs
+++ Program.cs
@@ -9,62 +9,7 @@ namespace Data_Structures_Mini_Projects
     {
         static void Main(string[] args)
         {
-            // Test My Stack 
-
-            MyStack<int> myStack = new MyStack<int>();
-
-            myStack.Push(10);
-            myStack.Push(20);
-            myStack.Push(30);
-            myStack.Push(40);
-            myStack.Push(50);
-
            
-            Console.WriteLine("\nPrint Stack Items");
-            myStack.Print();
-
-            myStack.Pop();
-            Console.WriteLine("\nStack after pop");
-            myStack.Print();
-
-            Console.WriteLine($"Stack size = {myStack.Size()}");
-            Console.WriteLine($"Stack front = {myStack.Top()}");
-            Console.WriteLine($"Stack back = {myStack.Bottom()}");
-
-            Console.WriteLine("______________________________________________________________");
-
-            // # Extension 1
-            Console.WriteLine("\n Item (2) = {0}", myStack.GetItem(2));
-
-            // # Extension 2
-            myStack.Reverse();
-            Console.WriteLine("\nStack after reverse");
-            myStack.Print();
-
-            // # Extension 3
-            myStack.UpdateItem(2, 600);
-            Console.WriteLine("\nStack after update item 2");
-            myStack.Print();
-
-            // # Extension 4
-            myStack.InsertAfter(2, 800);
-            Console.WriteLine("\nStack after insert 800 after item(2) ");
-            myStack.Print();
-
-            // # Extension 5
-            myStack.InsertAtFront(1000);
-            Console.WriteLine("\nStack after insert 1000 at front ");
-            myStack.Print();
-
-            // # Extension 6
-            myStack.InsertAtEnd(2000);
-            Console.WriteLine("\nStack after insert 2000 at end ");
-            myStack.Print();
-
-            // # Extension 7
-            myStack.Clear();
-            Console.WriteLine("\nStack after Clear() ");
-            myStack.Print();
 
         }
     }
--- project 5/MyDynamicArray.cs
+++ project 5/MyDynamicArray.cs
@@ -1,13 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Data_Structures_Mini_Projects.project_4
 {
-    public class MyDynamicArray
+    public class MyDynamicArray<T>
     {
+        private T[] _array;
+        private int _size;
+        public MyDynamicArray(int size)
+        {
+            if (size < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(size), "The size must be a non-negative number.");
+            }
+            _array = new T[size];
+            _size = size;
+        }
+        public void SetItem(int index, T item)
+        {
+            if (index >= _size)
+                throw new ArgumentOutOfRangeException(nameof(index), "The index must be less than the size of array");
+            _array[index] = item;
+        }
+        public void Print()
+        {
+            Console.WriteLine("Array Items :");
+            foreach (T item in _array)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
 
+        public bool IsEmpty()
+        {
+            return _size == 0;
+        }
+
+        public int Size()
+        {
+            return _size;
+        }
     }
 }
