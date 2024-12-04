7a7fbcd1ccaebb5aec7410c4abdc17cfea8a84c5
ZYAD-19 <zyadmahmod19720@gmail.com>
2024-12-04 06:33:47 +0200

    Implement UndoRedoManager: Generic class for managing undo/redo operations

--- Program.cs
+++ Program.cs
@@ -3,70 +3,44 @@ using Data_Structures_Mini_Projects.project_3;
 using Data_Structures_Mini_Projects.project_4;
 using Data_Structures_Mini_Projects.project_6;
 using Data_Structures_Mini_Projects.project_7;
+using System.Collections;
 
 
 namespace Data_Structures_Mini_Projects
 {
-    internal class Program
+    internal partial class Program
     {
         static void Main(string[] args)
         {
-            // Test My Stack 
+            //  Test The methods
 
-            MyStackArr<int> myStack = new MyStackArr<int>();
+            UndoRedoStack<string> undoRedo = new UndoRedoStack<string>();
+            undoRedo.Print();
+            undoRedo.Value = "1";
+            undoRedo.Print();
 
-            myStack.Push(10);
-            myStack.Push(20);
-            myStack.Push(30);
-            myStack.Push(40);
-            myStack.Push(50);
+            undoRedo.Value = "2";
+            undoRedo.Print();
 
+            undoRedo.Value = "3";
+            undoRedo.Print();
 
-            Console.WriteLine("\nPrint Stack Items");
-            myStack.Print();
+            Console.WriteLine("\n\nApply undo");
+            undoRedo.Undo();
+            undoRedo.Print();
+            undoRedo.Undo();
+            undoRedo.Print();
+            undoRedo.Undo();
+            undoRedo.Print();
 
-            myStack.Pop();
-            Console.WriteLine("\nStack after pop");
-            myStack.Print();
+            Console.WriteLine("\n\nApply redo");
+            undoRedo.Redo();
+            undoRedo.Print();
+            undoRedo.Redo();
+            undoRedo.Print();
+            undoRedo.Redo();
+            undoRedo.Print();
 
-            Console.WriteLine($"Stack size = {myStack.Size()}");
-            Console.WriteLine($"Stack front = {myStack.Top()}");
-            Console.WriteLine($"Stack back = {myStack.Bottom()}");
-
-            Console.WriteLine("______________________________________________________________");
-
-            // # Extension 1
-            Console.WriteLine("\n Item (2) = {0}", myStack.GetItem(2));
-
-            // # Extension 2
-            myStack.Reverse();
-            Console.WriteLine("\nStack after reverse");
-            myStack.Print();
-
-            // # Extension 3
-            myStack.UpdateItem(2, 600);
-            Console.WriteLine("\nStack after update item 2");
-            myStack.Print();
-
-            // # Extension 4
-            myStack.InsertAfter(2, 800);
-            Console.WriteLine("\nStack after insert 800 after item(2) ");
-            myStack.Print();
-
-            // # Extension 5
-            myStack.InsertAtFront(1000);
-            Console.WriteLine("\nStack after insert 1000 at front ");
-            myStack.Print();
-
-            // # Extension 6
-            myStack.InsertAtEnd(2000);
-            Console.WriteLine("\nStack after insert 2000 at end ");
-            myStack.Print();
-
-            // # Extension 7
-            myStack.Clear();
-            Console.WriteLine("\nStack after Clear() ");
-            myStack.Print();
 
         }
     }
---
+++ project 8/UndoRedoStack.cs
@@ -0,0 +1,57 @@
+﻿namespace Data_Structures_Mini_Projects
+{
+    internal partial class Program
+    {
+        public class UndoRedoStack<T>
+        {
+            private Stack<T> _undo;
+            private Stack<T> _redo;
+            private T _value;
+
+            public UndoRedoStack()
+            {
+                _undo = new Stack<T>();
+                _redo = new Stack<T>();
+                _undo.Push(_value);
+            }
+
+            public T Value
+            {
+                get { return _value; }
+                set
+                {
+
+                    if (_value == null || (!_value.Equals(value.ToString())))
+                    {
+                        _value = value;
+                        _undo.Push(_value);
+                    }
+                }
+            }
+
+            public void Undo()
+            {
+
+                if (_undo.Count > 0)
+                {
+                    _redo.Push(_value);
+                    _undo.Pop();
+                    _value = _undo.Peek();
+                }
+            }
+
+            public void Redo()
+            {
+                if (_redo.Count > 0)
+                {
+                    _value = _redo.Peek();
+                    _undo.Push(_value);
+                    _redo.Pop();
+                }
+
+            }
+
+            public void Print() => Console.WriteLine($" value = {Value}");
+        }
+    }
+}
